02f99c5a759a3efcbbb9d396d4b8e577c9fb1428
moududulhaque <moududulhaque@gmail.com>
2024-02-20 21:16:22 +0600

    BG Random COlor Chnager by Javascript

---
+++ checker.js
@@ -0,0 +1,19 @@
+<div id="hero" class="hero min-h-screen" style="background-color: ">
+<div class="hero-overlay bg-opacity-60"></div>
+<div class="hero-content text-neutral-content">
+  <div class="max-w-md">
+    <ul class="mb-5 ">
+      <h1 class="text-5xl font-bold">Key concepts covered:</h1>
+      <li class="mb-5">arrays</li>
+      <li class="mb-5">document.getElementById()</li>
+      <li class="mb-5">document.querySelector()</li>
+      <li class="mb-5">addEventListener()</li>
+      <li class="mb-5">document.body.style.backgroundColor</li>
+      <li class="mb-5">Math.floor()</li>
+      <li class="mb-5">Math.random()</li>
+      <li class="mb-5">array.length</li>
+    </ul>
+    <button id="change-color" class="btn btn-primary">Get Started</button>
+  </div>
+</div>
+</div>
